18644d6ee857c73fa1a188f29f7e474aff49dedb
Theodoro Cardoso <cardoso.theodoro@gmail.com>
2022-02-17 18:35:15 +0000

    fix pointer declaration to match Google C++ Style Guide

--- src/route_model.h
+++ src/route_model.h
@@ -28,8 +28,8 @@ class RouteModel : public Model {
 
       private:
         int index;
-        Node * FindNeighbor(std::vector<int> node_indices);
-        RouteModel * parent_model = nullptr;
+        Node* FindNeighbor(std::vector<int> node_indices);
+        RouteModel* parent_model = nullptr;
     };
 
     RouteModel(const std::vector<std::byte> &xml);
